09972e12b1c750ac496d66a619b5cf688ff61e00
Marco M. von Hagen <M2vH@users.noreply.github.com>
2017-12-15 12:25:11 +0100

    Update xmlScripts.js

--- assets/scripts/xmlScripts.js
+++ assets/scripts/xmlScripts.js
@@ -1,6 +1,37 @@
 // m2vh
-// makeBaseUri( xmlElement )
+// makeBaseUri( svg )
 
-makeBaseUri ( xml ) {
+// return an <img>-element with src=baseUri
+function imageWithBaseUri( svg ) {
+  var myImg;
+  myImg = new Image();
+  myImg.width = svg.width.baseVal.width;
+  myImg.height = svg.height.baseVal.height;
+  myImg.src = 'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(svg.outerHTML)));
   
+  return myImg;
+}
+
+// append an <img> to element#id
+function appendImage(id, img) {
+  document.getElementById(id).appendChild(img);
+} 
+
+// draw <img#id> into canvas
+// and replace with png
+function drawCanvas(img, id) {
+  var myCanvas, myCtx;
+  
+  myCanvas = document.createElement('canvas');
+  myCanvas.width = img.width;
+  myCanvas.height = img.height;
+  myCtx = myCanvas.getContext('2d');
+  myCtx.drawImage(img, 0, 0)
+  
+  var png = new Image();
+  png.src = myCanvas.toDataURL('image/png');
+  if (id) {
+    png.id = "id";
+  }
+  img.parentNode.replace(png, img)
 }
